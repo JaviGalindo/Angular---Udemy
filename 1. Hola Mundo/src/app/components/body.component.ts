91c94debce2387bfbb45b2e6246881bbb046a42a
JaviGalindo <javi.galindo.1991@hotmail.com>
2018-02-20 14:07:09 +0000

    Seccion 3
    Learned:
    - ngif and ngfor directives.
    - Bootstrap classes.

--- 1. Hola Mundo/src/app/components/body.component.ts
+++ 1. Hola Mundo/src/app/components/body.component.ts
@@ -5,5 +5,11 @@ import { Component } from "@angular/core";
     templateUrl: "body.component.html" 
 })
 export class BodyComponent {
+    mostrar:boolean = false;
+    frase:any = {
+        mensaje:"Un gran poder bla bla bla",
+        autor:"Ven al parque"
+    }
 
+    personajes:string[] = ["Spiderman", "Venon", "Octopus"];
 }
