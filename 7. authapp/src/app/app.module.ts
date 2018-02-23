6b073f36c5e3b27613b3daa0b5035bec57967169
JaviGalindo <javi.galindo.1991@hotmail.com>
2018-02-23 11:08:35 +0000

    Seccion 9
    - First commit
    - First structure AuthApp

---
+++ 7. authapp/src/app/app.module.ts
@@ -0,0 +1,30 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+
+import { APP_ROUTES } from './app.routes';
+
+
+import { AppComponent } from './app.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { HomeComponent } from './components/home/home.component';
+import { PricesComponent } from './components/prices/prices.component';
+import { ProtectedComponent } from './components/protected/protected.component';
+
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    NavbarComponent,
+    HomeComponent,
+    PricesComponent,
+    ProtectedComponent
+  ],
+  imports: [
+    BrowserModule,
+    APP_ROUTES
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
