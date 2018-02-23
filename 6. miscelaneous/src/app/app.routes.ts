667f173040c9204cdcdf10923a39f3411729955e
JaviGalindo <javi.galindo.1991@hotmail.com>
2018-02-23 10:20:04 +0000

    Seccion 8
    - Better routes approach

--- 6. miscelaneous/src/app/app.routes.ts
+++ 6. miscelaneous/src/app/app.routes.ts
@@ -2,18 +2,12 @@ import { Routes, RouterModule } from '@angular/router';
 
 import { HomeComponent } from './components/home/home.component';
 import { UsuarioComponent } from './components/usuario/usuario.component';
-import { UsuarioEditarComponent } from './components/usuario/usuario-editar.component';
-import { UsuarioNuevoComponent } from './components/usuario/usuario-nuevo.component';
-import { UsuarioDetalleComponent } from './components/usuario/usuario-detalle.component';
+import { USUARIO_ROUTES } from "./components/usuario/usuario.routes";
+
 
 const routes: Routes = [
     { path: 'home', component: HomeComponent },
-    { path: 'usuario/:id', component: UsuarioComponent, children: [
-        { path: 'new', component: UsuarioNuevoComponent },
-        { path: 'edit', component: UsuarioEditarComponent },
-        { path: 'detail', component: UsuarioDetalleComponent }
-        { path: '**', pathMatch:'full', redirectTo: 'new' }
-    ] },
+    { path: 'usuario/:id', component: UsuarioComponent, children: USUARIO_ROUTES},
     { path: '**', pathMatch:'full', redirectTo: 'home' }
 
     //{ path: 'path/:routeParam', component: MyComponent },
