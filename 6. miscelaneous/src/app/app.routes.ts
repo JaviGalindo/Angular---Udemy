609c5bad69c69608d58d8cc1ffe3947bb75517c7
JaviGalindo <javi.galindo.1991@hotmail.com>
2018-02-23 10:14:57 +0000

    seccion 8
    - Children routes first aproach

--- 6. miscelaneous/src/app/app.routes.ts
+++ 6. miscelaneous/src/app/app.routes.ts
@@ -2,10 +2,18 @@ import { Routes, RouterModule } from '@angular/router';
 
 import { HomeComponent } from './components/home/home.component';
 import { UsuarioComponent } from './components/usuario/usuario.component';
+import { UsuarioEditarComponent } from './components/usuario/usuario-editar.component';
+import { UsuarioNuevoComponent } from './components/usuario/usuario-nuevo.component';
+import { UsuarioDetalleComponent } from './components/usuario/usuario-detalle.component';
 
 const routes: Routes = [
     { path: 'home', component: HomeComponent },
-    { path: 'usuario', component: UsuarioComponent },
+    { path: 'usuario/:id', component: UsuarioComponent, children: [
+        { path: 'new', component: UsuarioNuevoComponent },
+        { path: 'edit', component: UsuarioEditarComponent },
+        { path: 'detail', component: UsuarioDetalleComponent }
+        { path: '**', pathMatch:'full', redirectTo: 'new' }
+    ] },
     { path: '**', pathMatch:'full', redirectTo: 'home' }
 
     //{ path: 'path/:routeParam', component: MyComponent },
