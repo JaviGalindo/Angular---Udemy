84b0fad6ffe81d985c544e2f141fac53b962f14c
JaviGalindo <javi.galindo.1991@hotmail.com>
2018-03-08 11:19:02 +0000

    forms section
    - form with template
    - form with data

--- 8. forms/src/app/components/data/data.component.ts
+++ 8. forms/src/app/components/data/data.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
-import { FormGroup, FormControl, Validators } from "@angular/forms";
+import { FormGroup, FormControl, Validators, FormArray } from "@angular/forms";
+import { Observable } from 'rxjs/Rx';
 
 @Component({
   selector: 'app-data',
@@ -14,7 +15,8 @@ export class DataComponent{
       name: "Javi",
       surname: "Munoz"
     },
-    email: "javi.munoz@AAAA.com"
+    email: "javi.munoz@aaaa.com",
+    // hobbies: ["Run", "Read", "Play"]
   }
   constructor() { 
     console.log(this.user);
@@ -22,17 +24,71 @@ export class DataComponent{
     this.forma = new FormGroup({
       'fullName': new FormGroup({
         'name': new FormControl(   '', [Validators.required, Validators.minLength(3)]),
-        'surname': new FormControl('', Validators.required),
+        'surname': new FormControl('', 
+                                    [ Validators.required,
+                                      this.noSurname
+                                    ]),
       }),
       'email': new FormControl(  '', [
                                       Validators.required, 
                                       Validators.pattern("[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,3}$")]),
+      'hobbies': new FormArray([
+        new FormControl('Run', Validators.required)
+      ]),
+      "username": new FormControl("", Validators.required, this.usernameExists),      
+      "password1": new FormControl("", Validators.required),
+      "password2": new FormControl(),
+
     });
 
+    // this.forma.setValue(this.user);
+    this.forma.controls["password2"].setValidators([
+      Validators.required,
+      this.noSame.bind(this.forma) //*******************quiero que this dentro de la funcion sea igual a this.forma
+    ]);
+
+    this.forma.valueChanges.subscribe(data => console.log(data));
+    this.forma.controls["username"].valueChanges.subscribe(data => console.log(data));
+    this.forma.controls["username"].statusChanges.subscribe(data => console.log(data));
+    
+
+  }
+  addHobbie() {
+    (<FormArray>this.forma.controls['hobbies']).push(
+      new FormControl("Sleep", Validators.required)
+    )
+  }
+
+  noSurname(control: FormControl):{[s:string]:boolean}{
+    if(control.value === "munoz"){
+      return {
+        nosurname:true
+      }
+    }
+    return null;
+  }
+
+  noSame(control: FormControl):{[s:string]:boolean}{
+    let forma:any = this;
+    if(control.value !== forma.controls["password1"].value){
+      return {
+        nosame:true
+      }
+    }
+    return null;
+  }
+
+  usernameExists(control:FormControl): Promise<any>|Observable<any>{
+    return new Promise((resolve, reject) => {
+      setTimeout(()=>{
+        if (control.value=="strider") return resolve({exist:true});
+        return resolve(null);
+      }, 3000)
+    })
   }
 
   guardarCambios(){
     console.log(this.forma.value);
-    
+    // this.forma.reset(this.user);
   }
 }
